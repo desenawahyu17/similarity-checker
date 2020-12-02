2ff88d02f438b90481698f4fa3e11a929b5676ad
Wahyu Desena <wahyudesena@gmail.com>
2020-12-02 16:11:20 +0700

    Semangat sayang

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>Similarity Checker</title>
+</head>
+<body>
+	<div class="header">
+  <a href="#" id="menu-action">
+    <i class="fa fa-bars"></i>
+    <span>Close</span>
+  </a>
+  <div class="logo">
+    Similarity Checker
+  </div>
+</div>
+<div class="sidebar">
+  <ul>
+    <li><a href="#"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
+    <li><a href="#"><i class="fa fa-server"></i><span>File Dokumen</span></a></li>
+    <li><a href="#"><i class="fa fa-file-text"></i><span>Preprocessing</span></a></li>
+    <li><a href="#"><i class="fa fa-book"></i><span>Similarity</span></a></li>
+</div> 
+<!-- Content -->
+<main class="main">
+<div class="row">
+	<div class="col-lg-4">
+      	<div class="panel panel-info">
+        	<div class="panel-heading" align="center"><h2>Data Upload</h2></div>
+        	<div class="panel-body" align="center">
+          		<i class="fa fa-user fa-3x"></i>
+          		<h2>Ada <?php echo 187; ?> Data</h2>
+        	</div>
+      	</div>
+    </div>
+    <div class="col-lg-4">
+      	<div class="panel panel-info">
+        	<div class="panel-heading" align="center"><h2>Data Preprocessing</h2></div>
+        	<div class="panel-body" align="center">
+          		<i class="fa fa-user fa-3x"></i>
+          		<h2>Ada <?php echo 180; ?> Data</h2>
+        	</div>
+      	</div>
+    </div>
+    <div class="col-lg-4">
+      	<div class="panel panel-info">
+        	<div class="panel-heading" align="center"><h2>Data Similarity</h2></div>
+        	<div class="panel-body" align="center">
+          		<i class="fa fa-user fa-3x"></i>
+          		<h2>Ada <?php echo 200; ?> Data</h2>
+        	</div>
+      	</div>
+    </div> 
+</div>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/style.js"></script>
+</main>
+</body>
+</html>
